dced4310e15f42b9192cbe9c65db54c6ede90ac3
MelDidntListen <melodyisnotlistening@gmail.com>
2025-04-24 14:45:10 -0700

    Added all current files
    added html, css, javascript, images

---
+++ translate.js
@@ -0,0 +1,111 @@
+let textInput = document.querySelector("textInput");
+
+//main func
+function typeName(){
+    currentName = "";
+    nameLetters = "";
+    nameLetterPS = "";
+    updateNameLetters();
+    // get translation func getTranslate()
+    // draw images
+};
+
+textInput.addEventListener("input", typeName);
+
+let currentName = InputEvent.data;
+
+// functions called in typeName() ============================================||
+// Makes the string into an array of letters
+function updateNameLetters(){
+    const nameLetters = currentName.split("");
+}
+
+//tester of current values
+console.log(nameLetters); // Output: ["H", "e", "l", "l", "o"]
+
+let nameLetterPS = 
+
+// functions called in saveEnter() ============================================||
+onkeyup = (Enter) => {};
+
+
+function saveEnter(){
+    console.log(currentName);
+}
+
+addEventListener("keyup", (Enter) => {});
+
+
+
+// functions called in clearName() ============================================||
+
+
+
+
+function clearName(){
+
+}
+
+addEventListener("keyup", (Delete) );
+addEventListener(document.querySelector("clear"));
+
+// Qould need delete, backspace (mobile), and button
+
+
+let rightHandLetters = {
+    "a" : "images/right_a.jpg",
+    "b" : "images/right_b.jpg",
+    "c" : "images/right_c.jpg",
+    "d" : "images/right_d.jpg",
+    "e" : "images/right_e.jpg",
+    "f" : "images/right_f.jpg",
+    "g" : "images/right_g.jpg",
+    "h" : "images/right_h.jpg",
+    "i" : "images/right_i.jpg",
+    "j" : "images/right_j.jpg",
+    "k" : "images/right_k.jpg",
+    "l" : "images/right_l.jpg",
+    "m" : "images/right_m.jpg",
+    "n" : "images/right_n.jpg",
+    "o" : "images/right_o.jpg",
+    "p" : "images/right_p.jpg",
+    "q" : "images/right_q.jpg",
+    "r" : "images/right_r.jpg",
+    "s" : "images/right_s.jpg",
+    "t" : "images/right_t.jpg",
+    "u" : "images/right_u.jpg",
+    "v" : "images/right_v.jpg",
+    "w" : "images/right_w.jpg",
+    "x" : "images/right_x.jpg",
+    "y" : "images/right_y.jpg",
+    "z" : "images/right_z.jpg",
+}
+
+let leftHandLetters = {
+    "a" : "images/left_a.jpg",
+    "b" : "images/left_b.jpg",
+    "c" : "images/left_c.jpg",
+    "d" : "images/left_d.jpg",
+    "e" : "images/left_e.jpg",
+    "f" : "images/left_f.jpg",
+    "g" : "images/left_g.jpg",
+    "h" : "images/left_h.jpg",
+    "i" : "images/left_i.jpg",
+    "j" : "images/left_j.jpg",
+    "k" : "images/left_k.jpg",
+    "l" : "images/left_l.jpg",
+    "m" : "images/left_m.jpg",
+    "n" : "images/left_n.jpg",
+    "o" : "images/left_o.jpg",
+    "p" : "images/left_p.jpg",
+    "q" : "images/left_q.jpg",
+    "r" : "images/left_r.jpg",
+    "s" : "images/left_s.jpg",
+    "t" : "images/left_t.jpg",
+    "u" : "images/left_u.jpg",
+    "v" : "images/left_v.jpg",
+    "w" : "images/left_w.jpg",
+    "x" : "images/left_x.jpg",
+    "y" : "images/left_y.jpg",
+    "z" : "images/left_z.jpg",
+}
